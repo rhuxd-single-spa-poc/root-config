f4ed4a44d73cbd17dd56d51163d503fca335e63c
Christie Molloy <cmolloy@redhat.com>
2020-07-22 16:29:41 -0400

    some updates

--- src/root-config.js
+++ src/root-config.js
@@ -5,17 +5,41 @@ import {
 } from "single-spa-layout";
 import { registerApplication, start } from "single-spa";
 
-registerApplication({
-  name: "@react-mf/navigation",
-  app: () => System.import("@react-mf/navigation"),
-  activeWhen: "/",
+const routes = constructRoutes(document.querySelector("#single-spa-layout"));
+const applications = constructApplications({
+  routes,
+  loadApp: ({ name }) => System.import(name),
+});
+// Delay starting the layout engine until the styleguide CSS is loaded
+const layoutEngine = constructLayoutEngine({
+  routes,
+  applications,
+  active: false,
 });
 
-registerApplication({
-  name: "@react-mf/apicurio-studio",
-  app: () => System.import("@react-mf/apicurio-studio"),
-  activeWhen: "/apicuriostudio",
-});
+applications.forEach(registerApplication);
+
+// registerApplication({
+//   name: "@react-mf/navigation",
+//   app: () => System.import("@react-mf/navigation"),
+//   activeWhen: "/",
+// });
+
+layoutEngine.activate();
+start();
+
+// const application = {
+//   bootstrap: () => Promise.resolve(), //bootstrap function
+//   mount: () => Promise.resolve(), //mount function
+//   unmount: () => Promise.resolve(), //unmount function
+// }
+// registerApplication('@react-mf/navigation', application);
+
+// registerApplication({
+//   name: "@react-mf/apicurio-studio",
+//   app: () => System.import("@react-mf/apicurio-studio"),
+//   activeWhen: "/apicuriostudio",
+// });
 
 // registerApplication({
 //   name: "@react-mf/syndesis",
@@ -27,27 +51,27 @@ registerApplication({
 //   return System.import('@react-mf/navigation');
 // }
 
-System.import("@react-mf/navigation");
-System.import("@react-mf/apicurio-studio");
-// System.import('@react-mf/syndesis');
+// System.import("@react-mf/navigation");
+// System.import("@react-mf/apicurio-studio");
+// // System.import('@react-mf/syndesis');
 
-const routes = constructRoutes(document.querySelector("#single-spa-layout"));
-const applications = constructApplications({
-  routes,
-  loadApp: ({ name }) => System.import(name),
-});
+// const routes = constructRoutes(document.querySelector("#single-spa-layout"));
+// const applications = constructApplications({
+//   routes,
+//   loadApp: ({ name }) => System.import(name),
+// });
 
-// Delay starting the layout engine until the styleguide CSS is loaded
-const layoutEngine = constructLayoutEngine({
-  routes,
-  applications,
-  active: false,
-});
+// // Delay starting the layout engine until the styleguide CSS is loaded
+// const layoutEngine = constructLayoutEngine({
+//   routes,
+//   applications,
+//   active: false,
+// });
 
-// applications.forEach(registerApplication);
+// // applications.forEach(registerApplication);
 
-layoutEngine.activate();
-start();
+// layoutEngine.activate();
+// start();
 
 // System.import("@react-mf/styleguide").then(() => {
 //   // Activate the layout engine once the styleguide CSS is loaded
